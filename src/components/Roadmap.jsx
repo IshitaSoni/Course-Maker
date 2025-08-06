058ffc72b29d815965a0d8764398cb70c55bf3a5
IshitaSoni <ishiworkpersonal@gmail.com>
2025-08-06 16:35:35 +0530

    css changes

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -6,19 +6,21 @@ const Roadmap = () => {
   return (
     <div className="roadmap">
       <h1>Python in 30 Days</h1>
-      {roadmap.weeks.map((week) => (
-        <div key={week.week} className="week-card">
-          <h2>Week {week.week}: {week.focus}</h2>
-          <ul className="day-list">
-            {week.days.map((day) => (
-              <li key={day.day} className="day-item">
-                <strong>Day {day.day}:</strong> {day.topic}
-                <p>{day.task}</p>
-              </li>
-            ))}
-          </ul>
+      <div className="weekcards">
+        {roadmap.weeks.map((week) => (
+            <div key={week.week} className="week-card">
+            <h2>Week {week.week}: {week.focus}</h2>
+            <ul className="day-list">
+                {week.days.map((day) => (
+                <li key={day.day} className="day-item">
+                    <strong>Day {day.day}:</strong> {day.topic}
+                    <p>{day.task}</p>
+                </li>
+                ))}
+            </ul>
         </div>
       ))}
+    </div>
     </div>
   );
 };
